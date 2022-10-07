3d0b1d495b30f5e88b8f9e870efaa72ecfd1bf32
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-07 18:21:23 +0300

    пределить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+Console.WriteLine("значение x:");
+int x = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("значение у:");
+int y = int.Parse(Console.ReadLine() ?? "0"); 
+
+if (x>0 && y>0)
+{
+    Console.WriteLine("1 черверть");
+
+}
+else if (x<0 && y>0)
+{
+    Console.WriteLine("2 четверть");
+
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine("3 четверть");
+}
+else 
+{
+    Console.WriteLine("4 четверть");
+}
