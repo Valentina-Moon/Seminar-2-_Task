cdd1ba1c0cb2c8bb31e664b2f117245117ce235d
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-07 17:55:39 +0300

    кратно ли число

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//кратно ли чесло 7 и 23
+Console.WriteLine("ведите число:");
+int a = int.Parse(Console.ReadLine());
+int b = 7;
+int c =23;
+
+if(a % b == 0 &&  a % c == 0)
+{
+Console.WriteLine("кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("не кратно 7 и 23");
+}
