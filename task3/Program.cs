5eb3aad6415f30071bc6604b5516aca04ebfd5f3
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-07 18:40:38 +0300

    проверка на полидром пятизначного числа

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// проверка на полидром пятизначного числа
+Console.WriteLine("Введите пятизначное число:");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int x5 = n%10;
+int x4 = n /10%10;
+int x2 = n /1000%10;
+int x1 = n / 10000;
+if ((x5 == x1) && (x4 == x2))
+{
+    Console.WriteLine(n + "полидром ");
+
+}
+else
+{
+     Console.WriteLine(n + " не является полидром ");
+}
